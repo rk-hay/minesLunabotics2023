b8cc326954190a0ec86178ceffbbfbd0914ba3ad
rk-hay <haydenbflem@gmail.com>
2024-04-30 18:45:33 -0600

    lo

--- depthai-ros/depthai_ros_driver/launch/rtabmap.launch.py
+++ depthai-ros/depthai_ros_driver/launch/rtabmap.launch.py
@@ -33,7 +33,7 @@ def launch_setup(context, *args, **kwargs):
             "Marker/Dictionary": "20", 
             "Marker/Priors": "9 1 4.58 .34 0 0 -1.57|10 7.14 4.58 .34 0 0 -1.57",
             "Marker/VarianceAngular": ".01",
-            "queue_size": "500",
+            "queue_size": 500,
             "rtabmapviz": "false"
             #Odometry: initial_pose
             #"Optimizer/PriorsIgnored": "False",
